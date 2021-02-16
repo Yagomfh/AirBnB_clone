ea6d99cb43662488dbe99477b70cf7c3f936c9dd
yagomfh <yagomfh@gmail.com>
2021-02-16 11:32:13 +0000

    feat: 1st commit

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,8 +4,29 @@ from io import StringIO
 from console import HBNBCommand
 from unittest.mock import patch
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
 import os.path
 from os import path
 
 
+class TestBaseDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Base class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(HBNBCommand, inspect.isfunction)
+
+    def test_pep8_conformance_base(self):
+        """Test that models/base.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstring(self):
+        """Tests for the module docstring"""
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
